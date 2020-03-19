f9d5b9466967132d31410938b8c15b8c97289eeb
nluizsoliveira <nluizsoliveira@gmail.com>
2020-03-19 05:29:14 -0300

    Beta version working!

--- Scrapy and Beautiful Soup/Scrapy/exemplo1/exemplo1/spiders/quotes_spyder.py
+++ Scrapy and Beautiful Soup/Scrapy/exemplo1/exemplo1/spiders/quotes_spyder.py
@@ -1,16 +1,41 @@
 import scrapy
+import datetime
 
+def start_url_generator():
+    base_url = 'http://www.economia.gov.br/agendas/gabinete-do-ministro/ministro-da-economia/paulo-guedes/'
+    start_date = datetime.date(2019, 1, 1)
+    end_date = datetime.date.today()
+    one_day = datetime.timedelta(days=1)
+
+    start_urls = []
+    while start_date < end_date:
+        year  = str(start_date.year).zfill(4)
+        month = str(start_date.month).zfill(2)
+        day   = str(start_date.day).zfill(2)
+
+        url_path = f'{year}-{month}-{day}?month:int={month}&year:int={year}'
+
+        start_urls.append(f'{base_url}{url_path}')
+        start_date += one_day
+
+    return start_urls
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/2/',
-    ]
+    start_urls = start_url_generator()
 
     def parse(self, response):
-        for quote in response.css('.quote'):  # Lista de Selectors
+        for compromisso in response.css('.item-compromisso'):  # Lista de Selectors
+
             yield {
-                "text": quote.css('.text::text').get(),
-                "author": quote.css('.author::text').get(),
-                "tags": quote.css('.tag ::text').getall(),
+                "MinistryName": response.css('.agenda-orgao::text').get(),
+                "MinisterName": response.css('.agenda-autoridade::text').get(),
+                "EventDate": compromisso.css('.horario.comprimisso-inicio').attrib['datetime'],
+                "EventTitle": compromisso.css('.comprimisso-titulo::text').get(),
+                "EventLocation": compromisso.css('.comprimisso-local ::text').getall()[2][1:], #Remove \n, Local: e espaço vazio
+                "EventParticipants" : [nome[:-1] for nome in compromisso.css('.comprimisso-participantes::text').getall()[1:]] 
             }
+
+    
+
+
